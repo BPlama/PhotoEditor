09dadea2d9080410cb4c06509cbd19b55be1529b
BPlama <lamaa12345lamaa@gmail.com>
2025-05-25 05:00:22 +0300

    General redesign using customtkinter

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-import tkinter
-from tkinter import *
+import tkinter as tk
+from tkinter import Menu
 import editor_logic as edl
+import customtkinter
 
-MAX_WIDTH = 800
-MAX_HEIGHT = 600
+MAX_WIDTH = 1000
+MAX_HEIGHT = 700
 
 start_x = start_y = 0
 rect_id = None
@@ -77,7 +78,7 @@ def start_crop():
     global cropping_mode
     cropping_mode = True
     canvas.config(cursor="cross")
-    info_label.config(text="Выделите область мышью...")
+    info_label.configure(text="Выделите область мышью...")
 
 
 def on_press(event):
@@ -95,7 +96,6 @@ def on_drag(event):
 
 def on_release(event):
     global cropping_mode, rect_id
-
     if not cropping_mode or not rect_id:
         return
 
@@ -116,50 +116,89 @@ def on_release(event):
 
     cropping_mode = False
     canvas.config(cursor="")
-    info_label.config(text="")
+    info_label.configure(text="")
+
 
 def undo_action():
     edl.undo()
     update_canvas()
 
 
+# --- GUI INIT ---
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
 
-# --- GUI ---
-window = Tk()
+window = customtkinter.CTk()
 window.title("Фоторедактор")
 window.geometry(f"{MAX_WIDTH}x{MAX_HEIGHT}")
 window.resizable(False, False)
 
-menu_bar = tkinter.Menu(window)
-file_menu = tkinter.Menu(menu_bar, tearoff=0)
-file_menu.add_command(label="Открыть", command=open_file)
-file_menu.add_command(label="Сохранить как", command=save_as)
-menu_bar.add_cascade(label="Файл", menu=file_menu)
-window.config(menu=menu_bar)
+# Меню
 
-side_panel = Frame(window, width=170, bg="#f0f0f0")
-side_panel.pack(side=LEFT, fill=Y, padx=10, pady=10)
+menu_style = {
+    "background": "#242632",
+    "activebackground": "#3A3B45"
+}
 
-button_width = 25
-Button(side_panel, text="Повернуть", command=rotate, width=button_width).pack(pady=5)
-Button(side_panel, text="Ч/Б", command=black_and_white, width=button_width).pack(pady=5)
-Button(side_panel, text="Отразить", command=flip, width=button_width).pack(pady=5)
-Button(side_panel, text="Сепия", command=sepia, width=button_width).pack(pady=5)
-Button(side_panel, text="Инверсия", command=invert, width=button_width).pack(pady=5)
-Button(side_panel, text="Яркость", command=brightness, width=button_width).pack(pady=5)
-Button(side_panel, text="Четкость", command=sharpness, width=button_width).pack(pady=5)
-Button(side_panel, text="Насыщенность", command=color, width=button_width).pack(pady=5)
-Button(side_panel, text="Обрезка", command=start_crop, width=button_width).pack(pady=5)
-Button(side_panel, text="Отменить", command=undo_action, width=button_width).pack(pady=5)
+menu_frame = tk.Frame(window, bg="#242632", height=30)
+menu_frame.pack(side="top", fill="x")
 
-info_label = Label(side_panel, text="", bg="#f0f0f0", wraplength=120)
-info_label.pack()
+# Кнопка "Файл"
+file_btn = tk.Menubutton(
+    menu_frame,
+    text="Файл",
+    **menu_style,
+    fg="white",
+    relief="flat",
+    bd=0
+)
+file_btn.pack(side="left", padx=10)
 
-image_frame = Frame(window, bg="gray")
-image_frame.pack(side=LEFT, fill=BOTH, expand=True)
+# Выпадающее меню для "Файл"
+file_dropdown = tk.Menu(
+    file_btn,
+    **menu_style,
+    fg="white",
+    tearoff=0
+)
 
-canvas = Canvas(image_frame, bg="gray", cursor="")
-canvas.pack(fill=BOTH, expand=True)
+file_dropdown.add_command(label="Открыть", command=open_file, **menu_style)
+file_dropdown.add_command(label="Сохранить как", command=save_as, **menu_style)
+
+file_btn.configure(menu=file_dropdown)
+
+# Боковая панель
+side_panel = customtkinter.CTkFrame(window, width=200, fg_color="#1a1a1a")
+side_panel.pack(side="left", fill="y", padx=10, pady=10)
+
+btn_style = {"width": 120,
+             "text_color": "white",
+             "fg_color": "#2E2F3A",
+             "hover_color": "#3D3E4A",
+             "border_width": 1,
+             "border_color": "#3D3E4A"
+             }
+
+customtkinter.CTkButton(side_panel, text="Повернуть", command=rotate, **btn_style).pack(pady=5)
+customtkinter.CTkButton(side_panel, text="Ч/Б", command=black_and_white, **btn_style).pack(pady=5)
+customtkinter.CTkButton(side_panel, text="Отразить", command=flip, **btn_style).pack(pady=5)
+customtkinter.CTkButton(side_panel, text="Сепия", command=sepia, **btn_style).pack(pady=5)
+customtkinter.CTkButton(side_panel, text="Инверсия", command=invert, **btn_style).pack(pady=5)
+customtkinter.CTkButton(side_panel, text="Яркость", command=brightness, **btn_style).pack(pady=5)
+customtkinter.CTkButton(side_panel, text="Четкость", command=sharpness, **btn_style).pack(pady=5)
+customtkinter.CTkButton(side_panel, text="Насыщенность", command=color, **btn_style).pack(pady=5)
+customtkinter.CTkButton(side_panel, text="Обрезка", command=start_crop, **btn_style).pack(pady=5)
+customtkinter.CTkButton(side_panel, text="Отменить", command=undo_action, **btn_style).pack(pady=5)
+
+info_label = customtkinter.CTkLabel(side_panel, text="", text_color="white", wraplength=120)
+info_label.pack(pady=5)
+
+# Canvas + контейнер для него
+image_frame = tk.Frame(window, bg="#1d1d27", highlightthickness=1, highlightbackground="#444")
+image_frame.pack(side="left", fill="both", expand=True, padx=5, pady=5)
+
+canvas = tk.Canvas(image_frame, bg="#1d1d27", bd=0, highlightthickness=0, relief="flat", cursor="")
+canvas.pack(fill="both", expand=True)
 
 canvas.bind("<ButtonPress-1>", on_press)
 canvas.bind("<B1-Motion>", on_drag)
